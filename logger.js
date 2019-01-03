34e8a249e4d7b15c6167be68e136f93189bd3510
Michael George <mgeorge9492@gmail.com>
2019-01-03 11:15:41 -0800

    I updated my code based on the code review today

--- logger.js
+++ logger.js
@@ -1,38 +1,19 @@
 'use strict';
 
-const fs = require('fs');
-const util = require('util');
 const io = require('socket.io-client');
-
 const socket = io.connect('http://localhost:3000');
 
-socket.on('read', read);
-socket.on('save', save);
-socket.on('error', errorCb);
+const logError = (payload) => {
+  if(payload) {
+    console.error('ERROR!', payload);
+  }
+};
 
-function errorCb(file) {
-  console.log('error');
-}
+const logSuccess = (payload) => {
+  if(payload) {
+    console.log('I think something was saved...', payload);
+  }
+};
 
-function save(file) {
-  console.log('file was saved');
-}
-
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-
-const toUpper = (buffer) => { return Buffer.from(buffer.toString().toUpperCase()); };
-
-function read(file) {
-  readFile('./new-text.txt', file)
-    .then((text) => {return toUpper(text);})
-    .then((text) => {writeFile('new-text.txt', text);})
-    .then(() => {
-      console.log(`${file} file saved`);
-      socket.emit('file-saved', file);
-    })
-    .catch((err) => {
-      console.log('error', err);
-      socket.emit('file-error', file);
-    });
-}
+socket.on('error', logError);
+socket.on('save', logSuccess);
